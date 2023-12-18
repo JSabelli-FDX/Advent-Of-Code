07f5fdadb1b1429f7d165710d1b2eca2d82b7e8c
FiRE-DRAG0N-X <sabell11@uwindsor.ca>
2023-12-18 01:19:24 -0500

    Part 1 and 2 done.

---
+++ 2023/Day 18/Part 1/main.py
@@ -0,0 +1,22 @@
+# Julia Sabelli
+# Advent of Code 2023
+# Day 18: Lavaduct Lagoon | Part 1
+
+points = [(0, 0)]
+dirs = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+
+b = 0
+
+with open('input.txt', 'r') as f:
+    for line in f:
+        d, n, _ = line.split()
+        dr, dc = dirs[d]
+        n = int(n)
+        b += n
+        r, c = points[-1]
+        points.append((r + dr * n, c + dc * n))
+
+A = abs(sum(points[i][0] * (points[i - 1][1] - points[(i + 1) % len(points)][1]) for i in range(len(points)))) // 2
+i = A - b // 2 + 1
+
+print(i + b)
