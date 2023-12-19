4be4843a3a7412d50058f9529db96bc7cbc5d181
FiRE-DRAG0N-X <sabell11@uwindsor.ca>
2023-12-19 01:58:37 -0500

    Part 1 and 2 done.

---
+++ 2023/Day 19/Part 1/main.py
@@ -0,0 +1,55 @@
+# Julia Sabelli
+# Advent of Code 2023
+# Day 19: Aplenty | Part 1
+
+def solve(file_name):
+    block1, block2 = open(file_name, 'r').read().split("\n\n")
+
+    workflows = {}
+
+    for line in block1.splitlines():
+        name, rest = line[:-1].split("{")
+        rules = rest.split(",")
+        workflows[name] = ([], rules.pop())
+        for rule in rules:
+            comparison, target = rule.split(":")
+            key = comparison[0]
+            cmp = comparison[1]
+            n = int(comparison[2:])
+            workflows[name][0].append((key, cmp, n, target))
+
+    ops = {
+        ">": int.__gt__,
+        "<": int.__lt__
+    }
+
+    def accept(item, name="in"):
+        if name == "R":
+            return False
+        if name == "A":
+            return True
+
+        rules, fallback = workflows[name]
+
+        for key, cmp, n, target in rules:
+            if ops[cmp](item[key], n):
+                return accept(item, target)
+
+        return accept(item, fallback)
+
+    total = 0
+
+    for line in block2.splitlines():
+        item = {}
+        for segment in line[1:-1].split(","):
+            ch, n = segment.split("=")
+            item[ch] = int(n)
+        if accept(item):
+            total += sum(item.values())
+
+    return total
+
+# Example usage:
+result = solve('input.txt')
+
+print(result)
