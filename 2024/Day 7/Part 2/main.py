055f19714cbace800328076becfd507998daa495
FiRE-DRAG0N-X <sabell11@uwindsor.ca>
2024-12-08 23:47:32 -0500

    Folder organization.

--- 2024/Day 7/Part 2/main.py
+++ 2024/Day 7/Part 2/main.py
@@ -10,28 +10,6 @@ lines = [
 ]
 print(len(lines))
 
-
-def dfs(val, i, nums, curr):
-    if i == len(nums) and curr == val:
-        return True
-    if i == len(nums):
-        return False
-
-    return dfs(val, i + 1, nums, curr + nums[i]) or dfs(
-        val, i + 1, nums, curr * nums[i]
-    )
-
-
-def part1():
-    res = 0
-    for line in lines:
-        test_val = line[0]
-        nums = line[1]
-        if dfs(test_val, 1, nums, nums[0]):
-            res += test_val
-    return res
-
-
 def dfs2(val, i, nums, curr):
     if curr > val:
         return False
@@ -56,6 +34,4 @@ def part2():
             res += test_val
     return res
 
-
-print(part1())
 print(part2())
